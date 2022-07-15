e6366c8367d8f4f4c81c96a5c7c0fde07cccb009
aditya1746 <68777899+aditya1746@users.noreply.github.com>
2022-07-16 01:16:38 +0530

    to include only_straight and only_depth

--- gui.py
+++ gui.py
@@ -2,40 +2,53 @@
 
 # from sympy import true
 import rospy
-
 from dynamic_reconfigure.server import Server
 from auv_codes2.cfg import thrusterConfig
 from std_msgs.msg import Int32MultiArray
 from std_msgs.msg import Int32
 from std_msgs.msg import String
+from std_msgs.msg import Float32
+from std_msgs.msg import Float64
+from only_straight import *
+from only_depth import *
 
+mode = 0
 
-pwmBase = 1470
-
-def callback_verify(data):
+def cb_verify(data):
     rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
 
+def depth_cb(msg):
 
+    if(mode == 3):
+        depthCBinGUI(msg.data)
 
-def callback_conf(config, level):
+def yaw_cb(msg):
+
+    if(mode == 4):
+        yawCBinGUI(msg.data)
+
+def cb_conf(config, level):
+    global mode
+
     rospy.loginfo("""Reconfigure Request: {servoFrontRight}, {servoFrontLeft},{servoMiddleRight}, {servoMiddleLeft}, {servoBackLeft}, {servoBackRight},{mode_thrusters}""".format(**config))
-   
+
     mode   = config.mode_thrusters
     pwm_fr, pwm_br, pwm_fl, pwm_bl, pwm_mr, pwm_ml =0, 0, 0, 0, 0, 0
-    if (mode==0):
-            #stop all thrusters
-        print('mode is ', mode)
-        pwm_fr, pwm_br, pwm_fl, pwm_bl, pwm_mr, pwm_ml =0, 0, 0, 0, 0, 0
 
-    elif  (mode==1):
-               #all thrusters value to 1600 to check every thruster is working
-        print('mode is ', mode)   
-        pwm_fr, pwm_br, pwm_fl, pwm_bl, pwm_mr, pwm_ml =80, 80, 80, 80, 80, 80
+    if (mode==0): #stop all thrusters
+        # print('mode is ', mode)
+        # pwm_fr, pwm_br, pwm_fl, pwm_bl, pwm_mr, pwm_ml =0, 0, 0, 0, 0, 0
+        stopCar()
+        stopMiddle()
 
-    elif  (mode==2):  
-        
-              # value receieved from reconfigure
-        print('mode is ', mode)
+    elif  (mode==1): #all thrusters value to 1600 to check every thruster is working
+        # print('mode is ', mode)   
+        # pwm_fr, pwm_br, pwm_fl, pwm_bl, pwm_mr, pwm_ml =80, 80, 80, 80, 80, 80
+        startCar()
+        startMiddle()
+
+    elif  (mode==2):   # value receieved from reconfigure
+        # print('mode is ', mode)
         pwm_fr = config.servoFrontRight
         pwm_fl = config.servoFrontLeft
         pwm_mr = config.servoMiddleRight
@@ -43,27 +56,10 @@ def callback_conf(config, level):
         pwm_br = config.servoBackRight
         pwm_bl = config.servoBackLeft
 
-    elif  (mode==3): 
-        print('mode is ', mode)
-
-    elif  (mode==4):         # value from imu sensor 
-
-        print('mode is ', mode)
-     
-        
-    elif  (mode==5):        # value from camera 
-        print('mode is ', mode)
-
     pwm_fr, pwm_br, pwm_fl, pwm_bl , pwm_mr, pwm_ml = pwmBase - int(pwm_fr), pwmBase +int(pwm_br),pwmBase + int(pwm_fl), pwmBase +int(pwm_bl), pwmBase + int(pwm_mr), pwmBase - int(pwm_ml)
     
     pwm_value= str(pwm_fr) +' '+ str(pwm_fl) +' '+str(pwm_mr)+ ' '+ str(pwm_ml)+ ' '+ str(pwm_br)+' '+ str(pwm_bl)
     pub.publish(pwm_value) 
-
-    print("Out of if else")
-    
-           
-    # pub7.publish(stop)
-
 
     return config
 
@@ -71,6 +67,9 @@ if __name__ == "__main__":
     rospy.init_node("thruster_value", anonymous = False)
     q = 1
     pub=rospy.Publisher("PWM_VALUE",String ,queue_size=q)
-    rospy.Subscriber("Verify", String, callback_verify)
-    srv = Server(thrusterConfig, callback_conf)
+
+    rospy.Subscriber("Depth", Float32, depth_cb)
+    rospy.Subscriber("angle_z", Float64, yaw_cb)
+    rospy.Subscriber("Verify", String, cb_verify)
+    srv = Server(thrusterConfig, cb_conf)
     rospy.spin()
